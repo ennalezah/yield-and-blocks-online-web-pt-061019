a2f64e1adf95f5a5e32a5d909ad103fecb470567
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-18 06:42:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,14 @@
 def hello_t(array)
-  i = 0 
+  if block_given?
+    i = 0 
   
-  while i < array.length 
-    yield array[i]
-    i += 1 
-  end 
+    while i < array.length 
+      yield array[i]
+      i += 1 
+    end 
   
   array
+  end
 end
 
 # call your method here!
